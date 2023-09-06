11985d75a69ff07d1fff100b3c42c6802b60cd7c
Cosmo Almeida <diegu.c@icloud.com>
2023-09-06 10:16:24 -0300

    Projeto - MeusLinks

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMode() {
+//Pegando o HTML:
+  const html = document.documentElement
+//Fazendo a troca das classes:
+  html.classList.toggle("light")
+//Pegando a imagem, mesmo seletor do CSS:
+  const img = document.querySelector("#profile img")
+//Substituir a imagem:
+  if (html.classList.contains("light")) {
+    img.setAttribute('src', './assets/avatar-light.png')
+  } else {
+    img.setAttribute('src', './assets/avatar.png')
+  }
+}
